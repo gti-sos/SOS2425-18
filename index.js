c9df65320dbdd2542e97ce14a655adb70f20c43f
MikyVaz <miguelvazquezdelarubia@gmail.com>
2025-03-02 17:55:23 +0100

    cambios index.js

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ const MVR= require("./samples/MVR/index-MVR.js");
 app.get("/samples/MVR", (request, response) => {
 
     const prov = "Castellón/Castelló";
-    const avg = MVR.avgByPrueb(prov);
+    const avg = MVR.avgByPrueb();
     response.send(`<!DOCTYPE html>
 <html lang="en">
 <head>
