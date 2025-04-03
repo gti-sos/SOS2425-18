03b57df61763acb918ea662f72e299d802478f2b
Miguel Ángel <migdomcie@alum.us.es>
2025-04-03 03:42:53 +0200

    busqueda implementada

--- src/back/index-MADC.js
+++ src/back/index-MADC.js
@@ -64,7 +64,7 @@ function loadBackendMADC(app){
     });
     
     //RETRIEVE
-    app.get(`${BASE_API}/${MADCmainResource}`, (request, response) => {
+    /*app.get(`${BASE_API}/${MADCmainResource}`, (request, response) => {
         let statusCode= 200;
 
         db_MADC.find({}, (err, data)=>{
@@ -85,13 +85,65 @@ function loadBackendMADC(app){
             }
         });
         
-    });
+    });*/
     
-    app.get(`${BASE_API}/${MADCmainResource}/:munName`, (request, response) => {
+    app.get(`${BASE_API}/${MADCmainResource}`, (request, response) => {
         let statusCode= 200;
         const munName= request.params.munName;
+        let q= {};
 
-        db_MADC.find({mun_name:`${munName}`}, (err, data)=>{
+        //year;month;grant_date;benef_id;benef_name;benef_type;
+
+        // purpose;grantor;grant_type;amt_granted;amt_paid;reimbursed;
+        // refunded;region_name;sec_cod;sec_descr;aid_type;reg_base;
+
+        // fund_local;fund_regional;fund_state;fund_eu;fund_other;fund_type;
+        // prov_name;mun_name
+        if(request.query.year) q.year= parseInt(request.query.year);
+        if(request.query.month) q.month= parseInt(request.query.month);
+        if(request.query.grant_date) q.grant_date= request.query.grant_date;
+        if(request.query.benef_id) q.benef_id= request.query.benef_id;
+        if(request.query.benef_name) q.benef_name= request.query.benef_name;
+        if(request.query.benef_type) q.benef_type= request.query.benef_type;
+
+        if(request.query.purpose) q.purpose= request.query.purpose;
+        if(request.query.grantor) q.grantor= request.query.grantor;
+        if(request.query.grant_type) q.grant_type= request.query.grant_type;
+        if(request.query.amt_granted) q.amt_granted= parseFloat(request.query.amt_granted);
+        if(request.query.amt_paid) q.amt_paid= parseFloat(request.query.amt_paid);
+        if(request.query.reimbursed) q.reimbursed= parseFloat(request.query.reimbursed);
+
+        if(request.query.refunded) q.refunded= parseFloat(request.query.refunded);
+        if(request.query.region_name) q.region_name= request.query.region_name;
+        if(request.query.sec_cod) q.sec_cod= parseInt(request.query.sec_cod);
+        if(request.query.sec_descr) q.sec_descr= request.query.sec_descr;
+        if(request.query.aid_type) q.aid_type= request.query.aid_type;
+        if(request.query.reg_base) q.reg_base= request.query.reg_base;
+
+        if(request.query.fund_local) q.fund_local= parseFloat(request.query.fund_local);
+        if(request.query.fund_regional) q.fund_regional= parseFloat(request.query.fund_regional);
+        if(request.query.fund_state) q.fund_state= parseFloat(request.query.fund_state);
+        if(request.query.fund_eu) q.fund_eu= parseFloat(request.query.fund_eu);
+        if(request.query.fund_other) q.fund_other= parseFloat(request.query.fund_other);
+        if(request.query.fund_type) q.fund_type= parseFloat(request.query.fund_type);
+
+        if(request.query.prov_name) q.prov_name= request.query.prov_name;
+        if(request.query.mun_name) q.mun_name= request.query.mun_name;
+
+        /*if (request.query.from) q.year.$gte = parseInt(request.query.from);
+        if (request.query.to) q.year.$lte = parseInt(request.query.to);
+
+        if (request.query.fromDate) q.grant_date.$gte = request.query.fromDate;
+        if (request.query.toDate) q.grant_date.$lte = request.query.toDate;
+        
+        const page = parseInt(request.query.page) || 1;
+        const limit = parseInt(request.query.limit) || 10;
+        const offset = (page - 1) * limit;
+
+        */
+
+        db_MADC.find(q, (err, data)=>{
+            console.log(data);
             if(err){
                 statusCode= 500;
                 return response.status(statusCode).json({"error": "Error interno del servidor", "statusCode": statusCode});
@@ -104,7 +156,7 @@ function loadBackendMADC(app){
                     return response.status(statusCode).json(data);
                 }else{
                     statusCode= 404;
-                    return response.status(statusCode).json({"error": `Recurso no encontrado para el municipio de ${munName}`, "statusCode": statusCode})
+                    return response.status(statusCode).json({"error": `Recurso no encontrado para el municipio de ${q.mun_name}`, "statusCode": statusCode})
                 }
             }
             
