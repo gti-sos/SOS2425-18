173cfb60f9afe0e3f929de34dd8a51993c13b2cb
GuilleBlanco08 <guille.blanco.diaz@gmail.com>
2025-04-20 14:05:55 +0200

    Modificada la vista http://sos2425-18.onrender.com/

--- index.js
+++ index.js
@@ -1,35 +1,38 @@
+//CONSTANTES DE DESPLIEGUE Y DEPENDENCIAS GENERALES
+//const express= require("express");
 import express from "express";
 import cors from "cors";
 import { loadBackendMADC } from "./src/back/index-MADC.js";
+//import { objData, pueblosdistintos, readAllDataMADC } from "./src/back/index-MADC.js";
+
 import { loadBackendGBD } from "./src/back/index-GBD.js";
 import { loadBackendMVR } from "./src/back/index-MVR.js";
+
+//import svelte-handler
 import { handler } from './src/front/build/handler.js';
 
-const app = express();
-const PORT = process.env.PORT || 3000;
-const PROYECTNAME = `SOS2425-18`;
-const isProduction = process.env.RENDER === "true" || process.env.NODE_ENV === "production";
+const app= express();
+const PORT= process.env.PORT || 3000;
+const PROYECTNAME= `SOS2425-18`;
+
 
 app.use(express.json());
 app.use(cors());
+//app.use("/", express.static("./public"));
 
-// APIs
 loadBackendMADC(app);
 loadBackendGBD(app);
 loadBackendMVR(app);
 
-// /about
-app.use("/about", express.static("./about/"));
-
-// SOLO en producción (Render) muestra landing page en "/"
-if (isProduction) {
-  app.use("/", express.static("./public"));
-}
-
-// Svelte
 app.use(handler);
 
-// Iniciar servidor
-app.listen(PORT, () => {
-  console.log(`Proyecto ${PROYECTNAME} desplegado en puerto ${PORT}`);
+// Readme
+app.use("/public", express.static("./public/"));
+app.use("/about", express.static("./about/"));
+
+// Inicializar el servidor
+app.listen(PORT, ()=>{
+    console.log(`Proyect ${PROYECTNAME} correctly deployed and running at port ${PORT}`);
 });
+
+
