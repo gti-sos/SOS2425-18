4062faf8a62f90b9689aa48e865f44d58735f2fb
Miguel Ángel <migdomcie@alum.us.es>
2025-04-20 18:18:14 +0200

    añadida funcionalidad D02 (a falta de pruebas e2e)

--- src/front/src/routes/Header.svelte
+++ src/front/src/routes/Header.svelte
@@ -1,4 +1,5 @@
-<h1>Front del grupo 18</h1>
+<h2>Front del grupo 18</h2>
 <a href="/">Home</a>
 <a href="/contr-mun-stats">Estadísticas sobre contrataciones en la dana</a>
 <a href="/dana-erte-stats">Estadísticas sobre el erte en la dana</a>
+<a href="/dana-grants-subsidies-stats">Estadísticas sobre ayudas durante la dana</a>
