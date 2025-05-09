3512590b7f4e1530b3865e3c8b848d39a6f6dc68
Miguel Ángel <migdomcie@alum.us.es>
2025-05-09 18:27:10 +0200

    modificado svelte.config.js

--- src/front/svelte.config.js
+++ src/front/svelte.config.js
@@ -1,10 +1,5 @@
 import adapter from '@sveltejs/adapter-node';
 
-const config = { kit: { adapter: adapter({
-                        env: {
-                            host: '0.0.0.0',
-                            port: process.env.PORT || 3000
-                        }
-}) } };
+const config = { kit: { adapter: adapter() } };
 
 export default config;
