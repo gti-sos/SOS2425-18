978d6b287cdb4c52b2ef6df1be70e01eac371988
Miguel Ángel <migdomcie@alum.us.es>
2025-05-09 19:09:46 +0200

    modificado vite.config

--- src/front/vite.config.js
+++ src/front/vite.config.js
@@ -6,18 +6,20 @@ console.log("SE esta ejecutando o no el vite!!!!!!!!");
 export default defineConfig({
 	plugins: [sveltekit()],
 	server: {
-		/*proxy: {
+		host: '0.0.0.0', // Requerido por Render
+		port: Number(process.env.PORT) || 3000, // Usa el puerto dinámico asignado por Render
+		fs: {
+			allow: [
+				'..',
+				'../../'
+			]
+		},
+		proxy: {
 			'/temp-api': {
 				target: 'https://sos2425-15.onrender.com',
 				changeOrigin: true,
 				rewrite: (path) => path.replace(/^\/temp-api/, '/api/v1/temperature-stats')
 			}
-		},*/
-		fs: {
-			allow: [
-				'..', // permite el directorio actual
-				'../../'
-			]
-		},
+		}
 	}
 });
