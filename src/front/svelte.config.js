bdf059adcd9768e6c795ad55e96ce82b2d3ab7f8
Miguel Ángel <migdomcie@alum.us.es>
2025-05-09 18:02:01 +0200

    modificado vite.config

--- src/front/svelte.config.js
+++ src/front/svelte.config.js
@@ -1,5 +1,10 @@
 import adapter from '@sveltejs/adapter-node';
 
-const config = { kit: { adapter: adapter() } };
+const config = { kit: { adapter: adapter({
+                        env: {
+                            host: '0.0.0.0',
+                            port: process.env.PORT || 3000
+                        }
+}) } };
 
 export default config;
--- src/front/vite.config.js
+++ src/front/vite.config.js
@@ -6,13 +6,13 @@ console.log("SE esta ejecutando o no el vite!!!!!!!!");
 export default defineConfig({
 	plugins: [sveltekit()],
 	server: {
-		proxy: {
+		/*proxy: {
 			'/temp-api': {
 				target: 'https://sos2425-15.onrender.com',
 				changeOrigin: true,
 				rewrite: (path) => path.replace(/^\/temp-api/, '/api/v1/temperature-stats')
 			}
-		},
+		},*/
 		fs: {
 			allow: [
 				'..', // permite el directorio actual
